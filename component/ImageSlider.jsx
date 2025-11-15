42b35fa4a4cd4c8f15df5cbbfa7fe3e2f3a076d4
Ujjwal Anand <ujjwalanand678@gmail.com>
2025-11-15 14:38:44 +0530

    small changes

--- component/ImageSlider.jsx
+++ component/ImageSlider.jsx
@@ -2,7 +2,6 @@
 
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
-
 import burgerImg from "@/assets/burger.jpg";
 import curryImg from "@/assets/curry.jpg";
 import dumplingsImg from "@/assets/dumplings.jpg";
